e1f47d7990162a0cbfb6369f5b1243b47f8f9816
Emerson Costa <syphonemerson@yahoo.com.br>
2020-03-15 16:19:54 -0300

    Melhorias
    Adicionado campo de imagem na tabela de usuarios
    Tirado partes que não serão utilizadas do index do admin

--- PI3/resources/views/admin/index.blade.php
+++ PI3/resources/views/admin/index.blade.php
@@ -12,16 +12,13 @@
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
 
     <link rel="stylesheet" href="admin_assets/css/admin.css">
-
 </head>
 
   <body>
      <!---Nav bar-->
       <nav class="navbar navbar-expand-md navbar-light">
-<button class="navbar-toggler ml-auto mb-2 bg-light" type="button" data-toggle="collapse" data-target="#navbar">
-<span class="navbar-toggler-icon"></span>
+<button class="navbar-toggler ml-auto mb-2 bg-light" type="button" data-toggle="collapse" data-target="#navbar"> <span class="navbar-toggler-icon"></span> </button>
 
-</button>
 <div class="collapse navbar-collapse" id="navbar">
     <div class="container-fluid">
         <div class="row">
@@ -36,33 +33,15 @@
                 </div>
 
                 <ul class="navbar-nav flex-column mt-4">
-                   <li class="nav-item"><a href="#" class="nav-link text-white p-3 mb-2
-                    current"><i class="fas fa-home text-light fa-lg mr-3"></i>
-                Home</a></li>
-                <li class="nav-item"><a href="#" class="nav-link text-white p-3 mb-2
-                    sidebar-link"><i class="fas fa-user text-light fa-lg mr-3"></i>
-                Profile</a></li>
-                <li class="nav-item"><a href="#" class="nav-link text-white p-3 mb-2
-                    sidebar-link"><i class="fas fa-envelope text-light fa-lg mr-3"></i>
-                inbox</a></li>
-                <li class="nav-item"><a href="#" class="nav-link text-white p-3 mb-2
-                    sidebar-link"><i class="fas fa-shopping-cart text-light fa-lg mr-3"></i>
-                Sales</a></li>
-                <li class="nav-item"><a href="#" class="nav-link text-white p-3 mb-2
-                    sidebar-link"><i class="fas fa-chart-line text-light fa-lg mr-3"></i>
-                Analytics</a></li>
-                <li class="nav-item"><a href="#" class="nav-link text-white p-3 mb-2
-                    sidebar-link"><i class="fas fa-chart-bar text-light fa-lg mr-3"></i>
-                charts</a></li>
-                <li class="nav-item"><a href="#" class="nav-link text-white p-3 mb-2
-                    sidebar-link"><i class="fas fa-table text-light fa-lg mr-3"></i>
-                Tables</a></li>
-                <li class="nav-item"><a href="#" class="nav-link text-white p-3 mb-2
-                    sidebar-link"><i class="fas fa-wrench text-light fa-lg mr-3"></i>
-                Settings</a></li>
-                <li class="nav-item"><a href="#" class="nav-link text-white p-3 mb-2
-                    sidebar-link"><i class="fas fa-file-alt text-light fa-lg mr-3"></i>
-                documentation</a></li>
+                    <li class="nav-item"><a href="#" class="nav-link text-white p-3 mb-2 current"> <i class="fas fa-home text-light fa-lg mr-3"></i> Home</a></li>
+
+                    <li class="nav-item"><a href="#" class="nav-link text-white p-3 mb-2 sidebar-link"><i class="fas fa-user text-light fa-lg mr-3"></i> Profile</a></li>
+
+                    <li class="nav-item"><a href="#" class="nav-link text-white p-3 mb-2 sidebar-link"><i class="fas fa-envelope text-light fa-lg mr-3"></i> inbox</a></li>
+
+                    <li class="nav-item"><a href="#" class="nav-link text-white p-3 mb-2 sidebar-link"><i class="fas fa-shopping-cart text-light fa-lg mr-3"></i> Sales</a></li>
+
+                    <li class="nav-item"><a href="#" class="nav-link text-white p-3 mb-2 sidebar-link"><i class="fas fa-wrench text-light fa-lg mr-3"></i> Settings</a></li>
                 </ul>
 
             </div>
@@ -390,219 +369,21 @@
 </section>
 
       <!--End of table-->
-      <!---Quick post-->
-      <section>
-          <div class="container-fluid">
-              <div class="row">
-                  <div class="col-xl-10 col-lg-9 col-md-8 ml-auto">
-                      <div class="row">
-                            <div class="col-xl-6 col-12">
-                                    <h4 class="text-dark p-3 mb-3">Tasks.</h4>
-                                    <div class="container-fluid bg-1">
-                                        <div class="row py-3 mb-4 task-border align-items-center">
-                                            <div class="col-1">
-                                                <input type="checkbox" checked>
 
-                                            </div>
-                                            <div class="col-sm-9 col-8">
-                                                Lorem ipsum dolor sit amet, consectetur
-                                                adipisicing elit.
-                                            </div>
-                                <div class="col-1">
-                                <a href="#" data-toggle="tooltip" title="Edit"
-                                data-placement="top">
-                                <i class="fas fa-edit fa-lg text-success mr-2">
-                                </i></a>
-                                            </div>
-                                <div class="col-1">
-                                <a href="#" data-toggle="tooltip" title="Delete"
-                                data-placement="top">
-                                <i class="fas fa-trash-alt fa-lg text-danger">
-                                </i></a>
-                                     </div>
-                                        </div>
-                                    </div>
-                                    <div class="container-fluid bg-2">
-                                      <div class="row py-3 mb-4 task-border align-items-center">
-                                          <div class="col-1">
-                                              <input type="checkbox" checked>
-
-                                          </div>
-                                          <div class="col-sm-9 col-8">
-                                              Lorem ipsum dolor sit amet, consectetur
-                                              adipisicing elit.
-                                          </div>
-                                <div class="col-1">
-                                <a href="#" data-toggle="tooltip" title="Edit"
-                                data-placement="top">
-                                <i class="fas fa-edit fa-lg text-success mr-2">
-                                </i></a>
-                                          </div>
-                                <div class="col-1">
-                                <a href="#" data-toggle="tooltip" title="Delete"
-                                data-placement="top">
-                                <i class="fas fa-trash-alt fa-lg text-danger">
-                                </i></a>
-                                   </div>
-                                      </div>
-                                  </div>
-                                  <div class="container-fluid bg-3">
-                                      <div class="row py-3 mb-4 task-border align-items-center">
-                                          <div class="col-1">
-                                              <input type="checkbox" checked>
-
-                                          </div>
-                                          <div class="col-sm-9 col-8">
-                                              Lorem ipsum dolor sit amet, consectetur
-                                              adipisicing elit.
-                                          </div>
-                                <div class="col-1">
-                                <a href="#" data-toggle="tooltip" title="Edit"
-                                data-placement="top">
-                                <i class="fas fa-edit fa-lg text-success mr-2">
-                                </i></a>
-                                          </div>
-                                <div class="col-1">
-                                <a href="#" data-toggle="tooltip" title="Delete"
-                                data-placement="top">
-                                <i class="fas fa-trash-alt fa-lg text-danger">
-                                </i></a>
-                                   </div>
-                                      </div>
-                                  </div>
-                                  <div class="container-fluid bg-4">
-                                      <div class="row py-3 mb-4 task-border align-items-center">
-                                          <div class="col-1">
-                                              <input type="checkbox" checked>
-
-                                          </div>
-                                          <div class="col-sm-9 col-8">
-                                              Lorem ipsum dolor sit amet, consectetur
-                                              adipisicing elit.
-                                          </div>
-                                <div class="col-1">
-                                <a href="#" data-toggle="tooltip" title="Edit"
-                                data-placement="top">
-                                <i class="fas fa-edit fa-lg text-success mr-2">
-                                </i></a>
-                                          </div>
-                                <div class="col-1">
-                                <a href="#" data-toggle="tooltip" title="Delete"
-                                data-placement="top">
-                                <i class="fas fa-trash-alt fa-lg text-danger">
-                                </i></a>
-                                   </div>
-                                      </div>
-                                  </div>
-                                  </div>
-                                  <!---End of Task List-->
-            <!---Quick Post-->
-         <div class="col-xl-6 mt-5">
-            <div class="card rounded">
-             <div class="card-body">
-                 <h5 class="text-dark text-center mb-4">
-                     Quick Status Post
-                   </h5>
-                <ul class="list-inline text-center py-3">
-                 <li class="list-inline-item">
-                     <a href="#"> <i class="fas fa-pencil-alt text-success"></i>
-                    <span class="h6 text-dark">Status</span>
-                    </a>
-                    </li>
-                    <li class="list-inline-item mr-4">
-                            <a href="#"> <i class="fas fa-camera text-info"></i>
-                           <span class="h6 text-dark">Photo</span>
-                           </a>
-                           </li>
-                           <li class="list-inline-item mr-4">
-                                <a href="#"> <i class="fas fa-map-marker text-danger"></i>
-                               <span class="h6 text-dark">Check In</span>
-                               </a>
-                               </li>
-
-                      </ul>
-                      <form>
-                          <div class="form-group">
-                              <input type="text" class="form-control py-2 mb-3"
-                              placeholder="Your Status">
-                              <button type="button" class="btn btn-block text-uppercase
-                              font-weight-bold text-light bg-button py-2 mb-5">Submit Post</button>
-                          </div>
-                      </form>
-                      <div class="row align-items-center mb-5">
-                          <div class="col-6">
-                              <div class="card bg-light">
-                                  <i class="far fa-calendar-alt fa-8x
-                                  text-info d-block m-auto py-3"></i>
-                                  <div class="card-body">
-                           <p class="card-text text-center font-weight-bold
-                           text-uppercase">Thu,Oct 10</p>
-                                  </div>
-                              </div>
-
-                          </div>
-                          <div class="col-6">
-                                <div class="card bg-light">
-                                        <i class="far fa-clock fa-8x
-                                        text-danger d-block m-auto py-3"></i>
-                                        <div class="card-body">
-                                 <p class="card-text text-center font-weight-bold
-                                 text-uppercase">2:15PM</p>
-                                        </div>
-                                    </div>
-                            </div>
-                      </div>
-
-                </div>
-              </div>
-
-
-       </div>
-           </div>
-       </div>
-              </div>
-
-          </div>
-      </section>
-
-      <!---End of Quick post-->
       <!---Footer-->
-<div class="container-fluid">
+<footer class="container-fluid">
     <div class="row">
         <div class="col-xl-10 col-lg-9 col-md-8 ml-auto mt-3 bd-bottom">
             <div class="row border-top pt-3">
-                <div class="col-lg-6 text-center">
-                    <ul class="list-inline">
-                <li class="list-inline-item mr-2">
-                    <a href="#" class="text-success">
-                        Company Name
-                    </a>
-                </li>
-                <li class="list-inline-item mr-2">
-                        <a href="#" class="text-success">
-                            About
-                        </a>
-                    </li>
-                    <li class="list-inline-item mr-2">
-                            <a href="#" class="text-success">
-                                Support
-                            </a>
-                        </li>
-                        <li class="list-inline-item mr-2">
-                                <a href="#" class="text-success">
-                                    Blog
-                                </a>
-                            </li>
-                </ul>
+
              </div>
                 <div class="col-lg-6 text-center text-success">
                     <p>&copy; Copyright. Senac 2020 - Sistemas para Internet - Projeto integrador 3</p>
-
-                    </div>
+                </div>
             </div>
         </div>
     </div>
-</div>
+</footer>
       <!---End of Footer-->
 
     <!-- Optional JavaScript -->
--- PI3/resources/views/auth/register.blade.php
+++ PI3/resources/views/auth/register.blade.php
@@ -61,15 +61,6 @@
                             </div>
                         </div>
 
-                        <div class="form-group row" >
-                            <label for="avatar" class='col-md-4 col-form-label text-md-right'>Imagem(Opcional)</label>
-
-                            <div class="col-md-6">
-                                <input type="file" class='form-control' name='avatar' id='avatar'>
-                            </div>
-
-                        </div>
-
                         <div class="form-group row mb-0">
                             <div class="col-md-6 offset-md-4">
                                 <button type="submit" class="btn btn-primary">
